68b28db5a4bdca6eda6302254c1779910c3598e5
Denis Monteiro <deniswmonteiro@gmail.com>
2023-08-28 00:58:41 -0300

    Ajuste no login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,23 +4,24 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { dbConnect } from "@/helpers/db-util";
 import { verifyPassword } from "@/helpers/auth-util";
 
-type CredentialsProps = {
-    email: string,
-    password: string
-}
-
 export const authOptions: NextAuthOptions = {
     session: {
         strategy: "jwt",
     },
     secret: process.env.NEXTAUTH_SECRET,
-    site: process.env.NEXTAUTH_URL,
     providers: [
         CredentialsProvider({
-            async authorize(credentials: CredentialsProps) {
+            type: "credentials",
+            credentials: {},
+            async authorize(credentials, req) {
+                const { email, password } = credentials as {
+                    email: string,
+                    password: string
+                }
+
                 const connect = await dbConnect();
                 const usersCollection = connect.db().collection("users");
-                const user = await usersCollection.findOne({ email: credentials.email });
+                const user = await usersCollection.findOne({ email });
 
                 if (!user) {
                     connect.close();
@@ -28,15 +29,16 @@ export const authOptions: NextAuthOptions = {
                 }
 
                 else {
-                    const password = await verifyPassword(credentials.password, user.password);
+                    const verifiedPassword = await verifyPassword(password, user.password);
 
-                    if (!password) {
+                    if (!verifiedPassword) {
                         connect.close();
                         throw new Error("Senha incorreta.");
                     }
 
                     else {
                         return {
+                            id: user.id,
                             name: user.name,
                             email: user.email,
                             image: null,
