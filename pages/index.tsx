3a13c68997a441fcc37e6f6bde912a3ab47fa33f
Denis Monteiro <deniswmonteiro@gmail.com>
2023-08-29 00:41:47 -0300

    Ajuste na página inicial

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,19 +40,19 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     let session: ISession | null = await getServerSession(context.req, context.res, authOptions);
     let user: IUserData | null = null;
     
-    // Get user data
-    if (session !== null) {
-        const userEmail = session.user.email;
-        const userReq = await fetch(`${process.env.NEXTAUTH_URL}/api/user/?email=${userEmail}`);
-        const userRes = await userReq.json() as IUserData;
+    // // Get user data
+    // if (session !== null) {
+    //     const userEmail = session.user.email;
+    //     const userReq = await fetch(`${process.env.NEXTAUTH_URL}/api/user/?email=${userEmail}`);
+    //     const userRes = await userReq.json() as IUserData;
         
-        if (userReq.ok) user = userRes;
+    //     if (userReq.ok) user = userRes;
 
-        else {
-            user = null;
-            session = null;
-        }
-    }
+    //     else {
+    //         user = null;
+    //         session = null;
+    //     }
+    // }
 
     return {
         props: {
